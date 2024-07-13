b11a8668108f0c40cfd4ac42e6207e50fccad8f0
HamaHs <lusuy1995@gmail.com>
2024-07-13 18:22:07 +0200

    rename css class

--- src/NotFound.tsx
+++ src/NotFound.tsx
@@ -3,7 +3,7 @@ import "./NotFound.scss";
 
 const NotFound = function ({ setBGColor }) {
    return (
-      <main className="not-found-main" onLoad={() => setBGColor("#F5F2F0")}>
+      <main className="main" onLoad={() => setBGColor("#F5F2F0")}>
          <section>
             <h1>Not found 404</h1>
          </section>
