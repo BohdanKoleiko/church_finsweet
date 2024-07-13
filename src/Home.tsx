b8c174d0f6182a64094823d11d8a58c92f9f469c
HamaHs <lusuy1995@gmail.com>
2024-07-13 18:18:09 +0200

    finish Home page

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Hero from "./components/Hero/Hero.tsx";
 import Heading from "./components/Heading/Heading.tsx";
 import Event from "./components/Event/Event.tsx";
@@ -7,23 +6,27 @@ import Button from "./components/Button/Button.tsx";
 import BlogCard from "./components/BlogCard/BlogCard.tsx";
 import ImgCards from "./components/ImgCards/ImgCards.tsx";
 import "./Home.scss";
+
 import { events } from "./BD/events.js";
 import { blogs } from "./BD/blogs.js";
+import arrow from "./images/icons/Arrow.svg";
+import bg1 from "./images/fashion-man-love-people.jpg";
+import bg2 from "./images/a-statue-holding-a-holy-book.jpg";
+import bg3 from "./images/man-people-woman-connection.jpg";
+import bg4 from "./images/woman-in-blue-tank-top-and-man-in-red-shirt-painting.jpg";
 
 const Home = function ({ setBGColor }) {
    return (
-      <main className="home-main" onLoad={() => setBGColor("white")}>
+      <main className="main" onLoad={() => setBGColor("white")}>
          <section className="home-hero">
             <Hero
-               backgroundImg="./images/hero-img.jpg"
-               backgroundPosition="center"
-               backgroundSize="cover"
-               backgroundRepeat="no-repeat"
+               background="url(./images/hero-img.jpg) center / cover no-repeat"
                button
                buttonTxt="learn more"
                headingType="h1"
                headingText="Become a part of our community"
                supHeadingText="Welcome to our CHURCH"
+               classNames="home-hero__heading"
             >
                <div className="home-hero__text">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.
@@ -48,26 +51,113 @@ const Home = function ({ setBGColor }) {
                   irure dolor in reprehenderit in voluptate velit esse cillum.
                </p>
 
-               <Button text="Read more" classNames="welcome__btn" />
+               <Button
+                  btn="primary"
+                  btnPadding="p24"
+                  btnType="button"
+                  text="Read more"
+                  classNames="welcome__btn"
+                  link="about-us"
+               />
 
                <ImgCards aligningItems="top" classNames="home__img-cards" />
+            </div>
+         </section>
 
-               <Heading
-                  headingTxt="celebrate WITH US"
-                  HeadingType="h4"
-                  textPosition="center"
-                  classNames="title title_margin-16"
-               >
-                  <div className="sup-title">our mission & vision</div>
-               </Heading>
-               <p className="wysiwyg wysiwyg_margin-16 wysiwyg_txt-center welcome__wysiwyg">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                  incididunt ut labore et dolore magna aliqua.
-               </p>
+         <section className="benefits-of-joining">
+            <div className="container">
+               <div className="benefits-of-joining__wrapper">
+                  <Heading
+                     HeadingType="h2"
+                     headingTxt="THE benefits of joining our church"
+                     textPosition="center"
+                     classNames="benefits-of-joining__title"
+                  >
+                     <div className="sup-title">Watch and listen</div>
+                  </Heading>
 
-               <Link to="#" className="welcome__link">
-                  Read More
-               </Link>
+                  <div className="cards-wrapper">
+                     <div
+                        className="card"
+                        style={{
+                           background: `url(${bg1}) center center/cover no-repeat`,
+                        }}
+                     >
+                        <div className="card__content-wrapper">
+                           <Heading
+                              HeadingType="h5"
+                              headingTxt="WATCH AND LISTEN TO OUR SERMONS"
+                              headingColor="white"
+                              classNames="card__title"
+                           />
+                           <p className="wysiwyg card__wysiwyg">
+                              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                              eiusmod tempor incididunt ut.
+                           </p>
+                        </div>
+                     </div>
+
+                     <div
+                        className="card"
+                        style={{
+                           background: `url(${bg2}) center center/cover no-repeat`,
+                        }}
+                     >
+                        <div className="card__content-wrapper">
+                           <Heading
+                              HeadingType="h5"
+                              headingTxt="WATCH AND LISTEN TO OUR SERMONS"
+                              headingColor="white"
+                              classNames="card__title"
+                           />
+                           <p className="wysiwyg card__wysiwyg">
+                              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                              eiusmod tempor incididunt ut.
+                           </p>
+                        </div>
+                     </div>
+
+                     <div
+                        className="card"
+                        style={{
+                           background: `url(${bg3}) center center/cover no-repeat`,
+                        }}
+                     >
+                        <div className="card__content-wrapper">
+                           <Heading
+                              HeadingType="h5"
+                              headingTxt="WATCH AND LISTEN TO OUR SERMONS"
+                              headingColor="white"
+                              classNames="card__title"
+                           />
+                           <p className="wysiwyg card__wysiwyg">
+                              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                              eiusmod tempor incididunt ut.
+                           </p>
+                        </div>
+                     </div>
+
+                     <div
+                        className="card"
+                        style={{
+                           background: `url(${bg4}) center center/cover no-repeat`,
+                        }}
+                     >
+                        <div className="card__content-wrapper">
+                           <Heading
+                              HeadingType="h5"
+                              headingTxt="WATCH AND LISTEN TO OUR SERMONS"
+                              headingColor="white"
+                              classNames="card__title"
+                           />
+                           <p className="wysiwyg card__wysiwyg">
+                              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                              eiusmod tempor incididunt ut.
+                           </p>
+                        </div>
+                     </div>
+                  </div>
+               </div>
             </div>
          </section>
 
@@ -94,11 +184,44 @@ const Home = function ({ setBGColor }) {
                      eventAltImg={events[events.length - 1].imgAlt}
                   >
                      <Button
+                        btn="secondary"
+                        btnType="button"
+                        btnPadding="p24"
                         text="Register"
                         classNames="seremon-preview__event-button"
-                        btn="secondary"
+                        link={`"sermon/register?id=${events[events.length - 1].id}`}
                      />
                   </Event>
+               </div>
+
+               <Button
+                  btn="text"
+                  text="View all Sermons"
+                  link="/sermon"
+                  logo={arrow}
+                  classNames="home-hero__all-sermons-button"
+               />
+            </div>
+         </section>
+
+         <section className="serve-the-world">
+            <div className="container">
+               <div className="serve-the-world__card">
+                  <div className="serve-the-world__card-content">
+                     <Heading HeadingType="h2" headingTxt="We want to serve the world around us" />
+                     <p className="wysiwyg wysiwyg_margin-16">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.
+                     </p>
+                     <Button
+                        btn="primary"
+                        btnType="button"
+                        btnPadding="p24"
+                        text="Visit"
+                        classNames="serve-the-world__button"
+                        link="/about-us"
+                     />
+                  </div>
+                  <div className="serve-the-world__card-background"></div>
                </div>
             </div>
          </section>
