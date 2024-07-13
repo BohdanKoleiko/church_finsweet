86146abc526be3c57cd5c535de005e9d73c4cf6c
HamaHs <lusuy1995@gmail.com>
2024-07-13 17:29:13 +0200

    remade button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,27 +1,66 @@
-import React, { FC } from "react";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Button.scss";
 
 interface ButtonProps {
-   btn?: "primary" | "secondary";
+   btn?: "primary" | "secondary" | "text";
    btnType?: "button" | "submit" | "reset";
    text?: string;
    btnPadding?: "p24" | "p20";
+   link?: string;
+   logo?: string;
    classNames?: string;
 }
 
-const Button: FC<ButtonProps> = function ({
-   btn = "primary",
+const Button = function ({
+   btn,
    btnType = "button",
-   text = "Button",
-   btnPadding = "p24",
+   text,
+   btnPadding,
+   link,
+   logo,
    classNames,
-}) {
-   return (
+}: ButtonProps) {
+   let navigate = useNavigate();
+
+   const handleRouteRedirection = function (path) {
+      navigate(path);
+      window.scrollTo(0, 0);
+   };
+
+   const buttonNoImageClasses = {
+      btn: btn ? `button_${btn}` : "",
+      btnPadding: btnPadding ? `button_${btnPadding}` : "",
+      classNames: classNames ? classNames : "",
+   };
+
+   const buttonClasses = {
+      btn: btn ? `button_${btn}` : "",
+      btnLogo: logo ? `button__logo` : "",
+      btnPadding: btnPadding ? `button_${btnPadding}` : "",
+      classNames: classNames ? classNames : "",
+   };
+
+   return !logo ? (
       <button
          type={btnType}
-         className={`button button__${btn} button_${btnPadding} ${classNames ? classNames : ""}`}
+         className={`button ${Object.values(buttonNoImageClasses)
+            .filter((value) => value)
+            .join(" ")}`}
+         onClick={() => handleRouteRedirection(link)}
       >
          {text}
+      </button>
+   ) : (
+      <button
+         type={btnType}
+         className={`button ${Object.values(buttonClasses)
+            .filter((value) => value)
+            .join(" ")}`}
+         onClick={() => handleRouteRedirection(link)}
+      >
+         <span className="button__text">{text}</span>
+         {logo && <img src={logo} alt="just icon" />}
       </button>
    );
 };
