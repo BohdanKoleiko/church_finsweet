154c111de89b59405d523c074d589cde1ddf6b20
HamaHs <lusuy1995@gmail.com>
2024-08-31 00:44:25 +0200

    own page of an event is finished

--- src/components/SingleEvent/SingleEvent.tsx
+++ src/components/SingleEvent/SingleEvent.tsx
@@ -1,68 +1,147 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Heading from "../Heading/Heading.tsx";
 import EventCard from "../EventCard/EventCard.tsx";
-import { ParagraphBlockProps } from "../SingleBlog/SingleBlog";
+import RegisterEventForm from "../Forms/RegisterEventForm.tsx";
+import { ParagraphBlockProps } from "../SingleBlog/SingleBlog.tsx";
 import "./SingleEvent.scss";
 
-export interface SingleEventProps {
+interface CertainEventProps {
    id: number;
-   eventName?: string;
-   startEventDate?: string;
-   endEventDate?: string;
-   eventVenue?: string;
-   eventDescr?: string;
-   img?: string;
-   imgAlt?: string;
+   eventName: string;
+   eventDescr: string;
+   startEventDate: string;
+   endEventDate: string;
+   eventVenue: string;
+   img: string;
+   imgAlt: string;
    finished: boolean;
-   body?: ParagraphBlockProps[];
+   paragraphBlock: ParagraphBlockProps[];
 }
 
-const SingleEvent = function ({ setBGColor, events, handleEventStatus }) {
+interface SingleEventProps {
+   setBGColor: (arg0: string) => {};
+   events: CertainEventProps[];
+   handleEventStatus: () => {};
+}
+
+const months: string[] = [
+   "January",
+   "February",
+   "March",
+   "April",
+   "May",
+   "June",
+   "July",
+   "August",
+   "September",
+   "October",
+   "November",
+   "December",
+];
+
+const SingleEvent = function ({ setBGColor, events, handleEventStatus }: SingleEventProps) {
+   const navigate = useNavigate();
+   const { sermonID } = useParams<{ sermonID: string }>();
+   const [certainEvent, setCertainEvent] = useState<CertainEventProps | null>(null);
+
+   useEffect(() => {
+      if (events) {
+         const fondEvent = events.find((event) => event.id === Number(sermonID));
+
+         if (!fondEvent) {
+            navigate("..", { relative: "path" });
+         }
+
+         setCertainEvent(fondEvent!);
+      }
+   }, [events, navigate, sermonID]);
+
+   if (!certainEvent) return null;
+
+   const startEventDate = new Date(certainEvent.startEventDate);
+   const startEventParsed = `${startEventDate.getDate().toString().padStart(2, "0")} ${
+      months[startEventDate.getMonth()]
+   }, ${startEventDate.getFullYear()}`;
+
    return (
-      events && (
-         <main className="main" onLoad={() => setBGColor("#F5F2F0")}>
-            <section className="single-event">
-               <div className="container">
-                  <div className="single-event__wrapper"></div>
-               </div>
-            </section>
+      <main className="main" onLoad={() => setBGColor("#F5F2F0")}>
+         <section className="single-event">
+            <div className="container">
+               <div className="single-event__wrapper">
+                  <div>
+                     <figure className="single-event__image-container">
+                        <img src={certainEvent.img} alt={certainEvent.imgAlt} />
+                     </figure>
 
-            <section>
-               <div className="container">
-                  {events && (
-                     <>
-                        <Heading
-                           headingTxt="Upcoming Sermons"
-                           HeadingType="h2"
-                           textPosition="center"
-                        />
-
-                        <div className="seremon-events__wrapper">
-                           {events
-                              .filter((event) => !event.finished)
-                              .map((event, index) => (
-                                 <Link to={"/sermons/" + event.id} key={index}>
-                                    <EventCard
-                                       startEventDate={event.startEventDate!}
-                                       endEventDate={event.endEventDate!}
-                                       eventName={event.eventName!}
-                                       eventDescription={event.eventDescr}
-                                       eventVenue={event.eventVenue}
-                                       handleEventStatus={handleEventStatus}
-                                       eventStatus={event.finished}
-                                       eventID={event.id}
-                                       backgroundColor="white"
-                                    />
-                                 </Link>
-                              ))}
+                     <Heading
+                        headingTxt={certainEvent.eventName}
+                        HeadingType="h2"
+                        classNames="single-event__title"
+                     >
+                        <div className="single-event__event-status sup-heading sup-heading_gradient">
+                           {certainEvent.finished ? "Finished event" : "Upcoming event"}
                         </div>
-                     </>
-                  )}
+                     </Heading>
+
+                     {certainEvent.paragraphBlock.map((pBlock, i) => {
+                        const TagType = (pBlock.textType as keyof JSX.IntrinsicElements) || "p";
+                        return (
+                           <TagType className="single-event__text" key={i}>
+                              {pBlock.text}
+                           </TagType>
+                        );
+                     })}
+                  </div>
+
+                  <div className="single-event__form-container">
+                     <RegisterEventForm
+                        venue={certainEvent.eventVenue}
+                        startEventDate={startEventParsed}
+                        eventStatus={certainEvent.finished}
+                        classNames={"single-event__form"}
+                     />
+                  </div>
                </div>
-            </section>
-         </main>
-      )
+            </div>
+         </section>
+
+         <section className="all-events">
+            <div className="container">
+               {events && (
+                  <>
+                     <Heading
+                        headingTxt="Upcoming Sermons"
+                        HeadingType="h2"
+                        textPosition="center"
+                        classNames="all-events__heading"
+                     />
+
+                     <div className="seremon-events__event-cards">
+                        {events
+                           .filter((event) => !event.finished)
+                           .map((event, index) => (
+                              <Link to={"/sermons/" + event.id} key={index}>
+                                 <EventCard
+                                    startEventDate={event.startEventDate!}
+                                    endEventDate={event.endEventDate!}
+                                    eventName={event.eventName!}
+                                    eventDescription={event.eventDescr}
+                                    eventVenue={event.eventVenue}
+                                    handleEventStatus={handleEventStatus}
+                                    eventStatus={event.finished}
+                                    eventID={event.id}
+                                    backgroundColor="white"
+                                 />
+                              </Link>
+                           ))}
+                     </div>
+                  </>
+               )}
+            </div>
+         </section>
+      </main>
    );
 };
 
