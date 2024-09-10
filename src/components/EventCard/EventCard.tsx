a3e7d106ef63fdf371950ea7b085042ec6fee2fc
HamaHs <lusuy1995@gmail.com>
2024-09-10 13:31:42 +0200

    removed repeated part of code and optimised entire structure

--- src/components/EventCard/EventCard.tsx
+++ src/components/EventCard/EventCard.tsx
@@ -17,11 +17,11 @@ export interface EventCardProps {
    eventStatus?: boolean;
    handleEventStatus?: Function;
    eventID?: number;
-   children?: any;
+   children?: React.ReactNode;
 }
 
-const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const months = [
+const dayList = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const monthList = [
    "January",
    "February",
    "March",
@@ -36,167 +36,74 @@ const months = [
    "December",
 ];
 
-const EventCard: FC<EventCardProps> = function (props) {
-   const {
-      classNames,
-      backgroundColor = "light-orange",
-      startEventDate,
-      endEventDate,
-      eventDescription,
-      eventVenue,
-      eventName,
-      preview,
-      eventImage,
-      eventAltImg,
-      eventStatus,
-      handleEventStatus,
-      eventID = 0,
-      children,
-   } = props;
+const formatDateTime = (date: Date) => {
+   const day = dayList[date.getDay()];
+   const month = monthList[date.getMonth()];
+   const hour = date.getHours().toString().padStart(2, "0");
+   const minute = date.getMinutes().toString().padStart(2, "0");
 
+   return { day, month, hour, minute };
+};
+
+const EventCard: FC<EventCardProps> = ({
+   classNames,
+   backgroundColor = "light-orange",
+   startEventDate,
+   endEventDate,
+   eventDescription,
+   eventVenue,
+   eventName,
+   preview,
+   eventImage,
+   eventAltImg,
+   eventStatus,
+   handleEventStatus,
+   eventID = 0,
+   children,
+}) => {
    const [isFinished, setIsFinished] = useState(false);
-   const eventStatusText = ["Upcoming event", "Finished event"];
-   const startEvent: Date = new Date(startEventDate);
-   const endEvent: Date = useMemo(() => new Date(endEventDate), [endEventDate]);
-   const [intervalTime, setIntervalTime] = useState(10);
+
+   const startEvent = useMemo(() => new Date(startEventDate), [startEventDate]);
+   const endEvent = useMemo(() => new Date(endEventDate), [endEventDate]);
+
+   const {
+      day: startDay,
+      month: startMonth,
+      hour: startHour,
+      minute: startMinute,
+   } = formatDateTime(startEvent);
+   const { day: endDay, hour: endHour, minute: endMinute } = formatDateTime(endEvent);
 
    useEffect(() => {
-      let intervalID: string | number | NodeJS.Timeout | undefined;
-
-      const handleActualTime = function () {
+      const handleActualTime = () => {
          const timeRemaining = endEvent.getTime() - Date.now();
          if (timeRemaining <= 0) {
             setIsFinished(true);
             handleEventStatus?.(eventID, true);
-            setIntervalTime(1000);
          }
       };
 
       if (!isFinished) {
-         intervalID = setInterval(() => {
-            handleActualTime();
-         }, intervalTime);
-      } else {
-         clearInterval(intervalID);
+         const intervalID = setInterval(handleActualTime, 1000);
+         return () => clearInterval(intervalID);
       }
+   }, [isFinished, eventID, handleEventStatus, endEvent]);
 
-      return () => clearInterval(intervalID);
-   }, [endEvent, eventID, handleEventStatus, intervalTime, isFinished]);
-
-   if (preview) {
-      return (
-         <div className={`event event-preview ${classNames}`}>
-            <div className={`event-preview__event-info event_${backgroundColor}`}>
-               <Heading classNames="event__heading" headingTxt={`${eventName}`} HeadingType="h5">
-                  <div className="event__sup-heading sup-heading">
-                     {eventStatus ? eventStatusText[1] : eventStatusText[0]}
-                  </div>
-               </Heading>
-
-               {eventDescription ? <p className="event__descr">{eventDescription}</p> : ""}
-
-               <div className="schedule">
-                  <div className="schedule__date">
-                     <figure className="schedule__image">
-                        <img
-                           src="../images/icons/clock.svg"
-                           alt="Icon illustrates clock as a symbol of upcoming event date"
-                        />
-                     </figure>
-                     <div>
-                        <div className="schedule__start-event">
-                           {startEvent
-                              ? `${days[startEvent.getDay()].trim()} ${startEvent.getHours()}:${
-                                   startEvent.getMinutes() === 0
-                                      ? "0" + startEvent.getMinutes()
-                                      : startEvent.getMinutes()
-                                } GMT`
-                              : "wrong data format"}
-                        </div>
-
-                        <div className="schedule__end-event">
-                           {endEvent
-                              ? `${days[endEvent.getDay()]} ${endEvent.getHours()}:${
-                                   endEvent.getMinutes() === 0
-                                      ? "0" + endEvent.getMinutes()
-                                      : endEvent.getMinutes()
-                                } GMT`
-                              : "wrong data format"}
-                        </div>
-                     </div>
-                  </div>
-
-                  <div className="schedule__destination">
-                     <figure className="schedule__image">
-                        <img
-                           src="../images/icons/destination.svg"
-                           alt="Icon illustrates point on the map"
-                        />
-                     </figure>
-                     {eventVenue ? <p>{eventVenue}</p> : ""}
-                  </div>
-               </div>
-
-               <div className="event__schedule">
-                  <span className="event__start-day">
-                     {startEvent.getDate().toString().length === 1
-                        ? `0${startEvent.getDate()}`
-                        : startEvent.getDate()}
-                  </span>
-                  <span className="event__start-month">{months[startEvent.getMonth()]}</span>
-               </div>
-
-               {children}
-            </div>
-
-            <figure className="event__img-wrapper preview-img-container">
-               <img className="preview-img-container__img" src={eventImage} alt={eventAltImg} />
+   const renderEventSchedule = () => (
+      <div className="schedule">
+         <div className="schedule__date">
+            <figure className="schedule__image">
+               <img
+                  src="../images/icons/clock.svg"
+                  alt="Icon illustrates clock as a symbol of upcoming event date"
+               />
             </figure>
+            <div>
+               <div className="schedule__start-event">{`${startDay} ${startHour}:${startMinute} GMT`}</div>
+               <div className="schedule__end-event">{`${endDay} ${endHour}:${endMinute} GMT`}</div>
+            </div>
          </div>
-      );
-   }
-
-   return (
-      <div className={`event event_${backgroundColor} ${classNames}`}>
-         <Heading classNames="event__heading" headingTxt={`${eventName}`} HeadingType="h5">
-            <div className="event__sup-heading sup-heading">
-               {eventStatus ? eventStatusText[1] : eventStatusText[0]}
-            </div>
-         </Heading>
-
-         {eventDescription ? <p className="event__descr">{eventDescription}</p> : ""}
-
-         <div className="schedule">
-            <div className="schedule__date">
-               <figure className="schedule__image">
-                  <img
-                     src="../images/icons/clock.svg"
-                     alt="Icon illustrates clock as a symbol of upcoming event date"
-                  />
-               </figure>
-               <div>
-                  <div className="schedule__start-event">
-                     {startEvent
-                        ? `${days[startEvent.getDay()].trim()} ${startEvent.getHours()}:${
-                             startEvent.getMinutes() === 0
-                                ? "0" + startEvent.getMinutes()
-                                : startEvent.getMinutes()
-                          } GMT`
-                        : "wrong data format"}
-                  </div>
-
-                  <div className="schedule__end-event">
-                     {endEvent
-                        ? `${days[endEvent.getDay()]} ${endEvent.getHours()}:${
-                             endEvent.getMinutes() === 0
-                                ? "0" + endEvent.getMinutes()
-                                : endEvent.getMinutes()
-                          } GMT`
-                        : "wrong data format"}
-                  </div>
-               </div>
-            </div>
-
+         {eventVenue && (
             <div className="schedule__destination">
                <figure className="schedule__image">
                   <img
@@ -204,20 +111,41 @@ const EventCard: FC<EventCardProps> = function (props) {
                      alt="Icon illustrates point on the map"
                   />
                </figure>
-               {eventVenue ? <p>{eventVenue}</p> : ""}
+               <p>{eventVenue}</p>
             </div>
-         </div>
+         )}
+      </div>
+   );
 
+   const renderHeader = () => (
+      <Heading classNames="event__heading" headingTxt={eventName} HeadingType="h5">
+         <div className="event__sup-heading sup-heading">
+            {eventStatus ? "Finished event" : "Upcoming event"}
+         </div>
+      </Heading>
+   );
+
+   return (
+      <div
+         className={`event ${
+            preview ? "event-preview" : ""
+         } event_${backgroundColor} ${classNames}`}
+      >
+         {renderHeader()}
+         {eventDescription && <p className="event__descr">{eventDescription}</p>}
+         {renderEventSchedule()}
          <div className="event__schedule">
             <span className="event__start-day">
-               {startEvent.getDate().toString().length === 1
-                  ? `0${startEvent.getDate()}`
-                  : startEvent.getDate()}
+               {String(startEvent.getDate()).padStart(2, "0")}
             </span>
-            <span className="event__start-month">{months[startEvent.getMonth()]}</span>
+            <span className="event__start-month">{startMonth}</span>
          </div>
-
          {children}
+         {preview && eventImage && (
+            <figure className="event__img-wrapper preview-img-container">
+               <img className="preview-img-container__img" src={eventImage} alt={eventAltImg} />
+            </figure>
+         )}
       </div>
    );
 };
